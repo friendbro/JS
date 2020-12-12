335f699712b1f4471c30145acb3e9d1b7ca60f10
friendbro <65781793+friendbro@users.noreply.github.com>
2020-12-12 10:09:31 +0530

    Update syllabus.js

--- syllabus.js
+++ syllabus.js
@@ -1,32 +1,35 @@
 function myFunction1() {
     document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("hide").style.display = "none";
     document.getElementById("demo").innerHTML = "First Flight:-<br>1.Glimpses of India<br>2.Madam Rides the Bus<br>3.The Sermon at Banaras<br>4.The Proposal<br><br> Foot Prints<br>1.The making of a scientist<br>2.The Necklace<br>3.The Hack driver<br>4.Bholi<br><br>Grammar<br>1.Modals<br>2.Determiners<br>3.Subject Verb Concord<br>4.Reported Speech<br>5.Tenses";
 }
 
 function myFunction2() {
-    document.getElementById("demo").innerHTML = "Not Available";
+    document.getElementById("demo").innerHTML = "गद्य:<br>अब कहाँ दूसरों के दुख से दुखी होने वाले<br><br>पद्य: <br>मनुष्यता <br><br>संचयन: <br>सपनों के से दिन<br><br>व्याकरण:<br>पदबंधऔर समाज";
     document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("hide").style.display = "none";
 }
 
 function myFunction3() {
     document.getElementById("demo").innerHTML = "Chapter 7 - Coordinate Geometry<br>Chapter 13 - Surface Area And Volume<br>Chater 15 - Statistics";
     document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("hide").style.display = "none";
 }
 
 function myFunction4() {
     document.getElementById("demo").innerHTML = "Biology:<br>Chapter 8 - How do Organisms Reproduce<br><br>Chemistry:<br>Chapter 3 - Metals and Non Metals(Till 3.3.1)<br><br>Physics<br>Chapter 11 - Light Reflection and Refraction";
     document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("hide").style.display = "none";
 }
 
 function myFunction5() {
     document.getElementById("demo").innerHTML = "History:<br>The Age of Industrialization<br><br>Geography:<br>Manufacturing Industries<br><br>Democratic Politics:<br>Outcomes of Democracy.<br><br>Economics:<br>Globalization of Indian Economic.";
     document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("hide").style.display = "none";
 }
 
 function myFunction6() {
     document.getElementById("demo").innerHTML = "Unit - 1 Digital documentation(MS Word)<br>Unit - 2 Electronic Spreadsheet";
     document.getElementById("demo").style.visibility = "visible";
+    document.getElementById("hide").style.display = "none";
 }
-
-
-
